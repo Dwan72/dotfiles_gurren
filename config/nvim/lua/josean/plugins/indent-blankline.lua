586c2ef5dd9125666c12ec034c133e86a9dded4f
Alex An <alean@cisco.com>
2025-04-30 15:26:18 -0500

    removing statusline and updating indent-blankline

--- config/nvim/lua/josean/plugins/indent-blankline.lua
+++ config/nvim/lua/josean/plugins/indent-blankline.lua
@@ -10,16 +10,22 @@ return {
   config = function()
     local highlight = {
       'someRed',
+      -- 'IblScope',
     }
     local hooks = require 'ibl.hooks'
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
       vim.api.nvim_set_hl(0, 'someRed', { fg = '#252530' })
+      vim.api.nvim_set_hl(0, 'IblScope', { fg = '#484857' })
     end)
 
     local ibl = require('ibl').setup {
       indent = {
         char = '▏',
         highlight = highlight,
+      },
+      scope = {
+        show_start = false,
+        show_end = false,
       },
     }
   end,
